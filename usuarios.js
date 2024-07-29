fcbe2aeba8a0684acd02c2811e74fb0b2dcac652
Neto <j.altevirneto@gmail.com>
2024-07-29 09:20:23 -0300

    feat

---
+++ usuarios.js
@@ -0,0 +1,55 @@
+const prompt = require("prompt-sync")();
+//adicionar nome, email, id, lista de telefones
+const usuarios = [];
+const ultimoID = 0;
+const modelo = () => {
+    const usuario = {};
+    while(true){
+        usuario.nome = prompt("Informe o nome de usuário: ")
+        if(usuario.nome == ""){
+            console.log("O nome não pode estar vazio!");
+        }else{
+            break;
+        }
+    }
+    while(true){
+        usuario.email = prompt("Informe o seu email: ");
+        if(usuario.email == ""){
+            console.log("O email não pode estar vazio!");
+        }else{
+            break;
+        }
+    }
+    while(true){
+        usuario.telefones = []
+        let telefone = prompt('Informe quantos telefones desejar, quando quiser parar digite "Não"').toLowerCase
+        if(telefone == "nao" || telefone == "não"){
+            break;
+        }else{
+            telefone = parseInt(telefone);
+            usuario.telefones.push(telefone);
+        }
+    }
+    usuario.id = ultimoID + 1;
+    return usuario;
+}
+const cadastro = () => {
+    const usuario = modelo();
+    usuarios.push(usuario);
+}
+const listagem = () => {
+
+}
+const atualizar = () => {
+
+}
+const deletar = () => {
+
+}
+
+module.exports = {
+    cadastro,
+    listagem,
+    atualizar,
+    deletar,
+}
